9ea6982a526c697bd250d01f25ebc9ecd736742e
Palm24 <62299587+Palm24@users.noreply.github.com>
2023-09-11 14:06:57 +0700

    fix maint.ts

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,24 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
 
-@Component({
-  selector: 'my-app',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h1>Hello from {{ name }}! My name is PMNTS24</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular
-    </a>
-  `,
-})
-export class App {
-  name = 'Angular';
-}
+// @Component({
+//   selector: 'my-app',
+//   standalone: true,
+//   imports: [CommonModule],
+//   template: `
+//     <h1>Hello from {{ name }}! My name is PMNTS24</h1>
+//     <a target="_blank" href="https://angular.io/start">
+//       Learn more about Angular
+//     </a>
+//   `,
+// })
+// export class App {
+//   name = 'Angular';
+// }
 
-bootstrapApplication(App);
+// bootstrapApplication(App);
+
+platformBrowserDynamic().bootstrapModule(AppModule);
